94d07138c6b7bf47e502a9740e1cc8a96598adf6
“Doraemonhub“ <18600553474@163.com>
2017-05-01 11:51:45 +0800

    修改关注界面无视频流时提示，修改分享图片

--- app/src/main/java/com/rose/guojiangzhibo/urlconfig/FourConfig.java
+++ app/src/main/java/com/rose/guojiangzhibo/urlconfig/FourConfig.java
@@ -8,4 +8,7 @@ public class FourConfig {
     public static String getRobot() {
         return "http://www.tuling123.com/openapi/api?key=080390332ce0704a54cd5a3aaf70f443&info=";
     }
+    public static String getShareUrl() {
+        return "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493620420793&di=076055df257aef90b5a715421ddf2291&imgtype=0&src=http%3A%2F%2Fimg.25pp.com%2Fuploadfile%2Fapp%2Ficon%2F20160330%2F1459339365815990.jpg";
+    }
 }
